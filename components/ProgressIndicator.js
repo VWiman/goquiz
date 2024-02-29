e2facbd1dc3bd368d1680a05f1530fbeb833f2fb
marwal1987 <marwal1987@gmail.com>
2024-02-29 08:24:19 +0100

    added logic to ProgressIndicator. Feel free to adapt or discard

--- components/ProgressIndicator.js
+++ components/ProgressIndicator.js
@@ -1,3 +1,18 @@
+import { QuizContext } from "../../context/QuizContext";
+
 export default function ProgressIndicator() {
-  return <p>More logic here</p>;
+  const { qaPairs } = useContext(QuizContext);
+  const totalQuestions = qaPairs.length;
+  const { answeredQuestions } = useContext(QuizContext);
+  const answeredCount = answeredQuestions.length;
+  const progress = (answeredCount / totalQuestions) * 100;
+
+  return (
+    <div>
+      <h2>Progress: {progress}%</h2>
+      <p>
+        Answered: {answeredCount} / {totalQuestions}
+      </p>
+    </div>
+  );
 }
