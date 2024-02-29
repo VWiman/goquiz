f06492044187e6444bd353e3ce06840945c6e1a7
why_code <119089018+VWiman@users.noreply.github.com>
2024-02-29 14:07:30 +0100

    Changed to one state for new question

--- context/QuizContext.js
+++ context/QuizContext.js
@@ -5,33 +5,25 @@ import { createContext, useState } from "react";
 export const QuizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
-  const [stateQuestions, setStateQuestions] = useState({ questions: [] });
-  const [question, setQuestion] = useState("");
-  const [answer, setAnswer] = useState("");
-  const [choiceTwo, setChoiceTwo] = useState("");
-  const [choiceThree, setChoiceThree] = useState("");
-  const [choiceFour, setChoiceFour] = useState("");
-  const [isSending, setIsSending] = useState(false);
-  return (
-    <QuizContext.Provider
-      value={{
-        stateQuestions,
-        setStateQuestions,
-        question,
-        setQuestion,
-        answer,
-        setAnswer,
-        choiceTwo,
-        setChoiceTwo,
-        choiceThree,
-        setChoiceThree,
-        choiceFour,
-        setChoiceFour,
-        isSending,
-        setIsSending,
-      }}
-    >
-      {children}
-    </QuizContext.Provider>
-  );
+	const [stateQuestions, setStateQuestions] = useState({ questions: [] });
+	const [newQuestion, setNewQuestion] = useState({question: "", answer: "", choiceTwo: "", choiceThree: "", choiceFour: ""});
+	// const [question, setQuestion] = useState("");
+	// const [answer, setAnswer] = useState("");
+	// const [choiceTwo, setChoiceTwo] = useState("");
+	// const [choiceThree, setChoiceThree] = useState("");
+	// const [choiceFour, setChoiceFour] = useState("");
+	const [isSending, setIsSending] = useState(false);
+	return (
+		<QuizContext.Provider
+			value={{
+				stateQuestions,
+				setStateQuestions,
+				newQuestion,
+				setNewQuestion,
+				isSending,
+				setIsSending,
+			}}>
+			{children}
+		</QuizContext.Provider>
+	);
 };
