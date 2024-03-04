8f1f0bebadff6767b51a0bcfddea4d0c9f3b8955
ozayo <ozay.ozdemir@gmail.com>
2024-03-04 13:57:07 +0100

    fixed css for responsive

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <header className="flex w-full h-[40px]  bg-blue-100   py-2 px-10 justify-center font-mono">
+    <footer className="flex w-full  bg-blue-100 py-2 px-10 justify-center font-mono">
       <p>2024 - GoQuiz - Chas Academy</p>
-    </header>
+    </footer>
   );
 };
 
