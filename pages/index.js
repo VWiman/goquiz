98b66d3199923c4055acd09c8fe7f75237cc1794
ozayo <ozay.ozdemir@gmail.com>
2024-03-04 13:58:37 +0100

    New design & structure

--- pages/index.js
+++ pages/index.js
@@ -8,31 +8,36 @@ export default function Home() {
   const [currentFact, setCurrentFact] = useState("");
 
   const facts = [
-    "The heart of a shrimp is located in its head.",
-    "A snail can sleep for three years.",
-    "Slugs have four noses.",
-    "Elephants are the only animal that can't jump.",
+    "Who wrote \"Romeo and Juliet\"?",
+    "What language is used to style web pages?",
+    "What is the largest planet in our solar system?",
+    "Who discovered gravity?",
+    "Which company created the JavaScript programming language?",
   ];
   const fetchRandomFact = () => {
     const randomIndex = Math.floor(Math.random() * facts.length);
     setCurrentFact(facts[randomIndex]);
   };
+
   useEffect(() => {
-    fetchRandomFact();
+    fetchRandomFact(); // İlk random text'i yüklenirken göster
+    const timer = setInterval(() => {
+      fetchRandomFact();
+    }, 3000);
+
+    return () => clearInterval(timer);
   }, []);
 
   return (
     <>
-      <main className="flex justify-center items-center min-h-screen font-mono bg-blue-200">
-        <div className="max-w-3xl mx-auto p-4">
-          <p className="mb-4 text-center sm:text-left">"{currentFact}"</p>
-          <h1 className=" mb-4 text-2xl font-extrabold text-center sm:text-left">
-            Welcome to GoQuiz
-          </h1>
-          <p className="text-center sm:text-left mb-4">
-            Please write your username below{" "}
-          </p>
-          <div className="flex flex-col sm:flex-row items-center gap-4 md:gap-10">
+      <main className="flex justify-center h-screen font-mono bg-blue-200 pt-14 px-3 md:px-0">
+        <div className="max-w-xl mx-auto">
+          <h1 className=" mb-8 text-3xl font-extrabold">Welcome to GoQuiz</h1>
+          <div className="bg-zinc-900 rounded-md -mb-4 py-1 px-3 text-white text-xs inline-block -ml-3">Do you know the answer?</div>
+          <p className="transition-opacity duration-1000 rounded-md bg-blue-100 text-black p-2">{currentFact}</p>
+          <h2 className="mt-5 text-xl font-extrabold">Can you achieve a perfect score?</h2>
+          <p className="text-xs mt-4 mb-4">Enter your username and click 'START' to begin. Choose from four options for each question. </p>
+          <div className="flex justify-center gap-5 md:gap-10 items-center mt-10 mb-8 flex-col md:flex-row">
             <input
               id="userNameInput"
               name="userNameInput"
