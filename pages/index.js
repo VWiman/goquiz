86e886cf8e29c9a7d8f664d7720ffcf2f8ccb852
marwal1987 <marwal1987@gmail.com>
2024-03-04 09:33:07 +0100

    Made index.js responsive

--- pages/index.js
+++ pages/index.js
@@ -24,12 +24,12 @@ export default function Home() {
 
   return (
     <>
-      <main className="flex justify-center items-center h-screen font-mono bg-blue-200">
-        <div className="max-w-5xl mx-auto">
-          <p>{currentFact}</p>
-          <h1 className=" mb-4 text-2xl font-extrabold">Welcome to GoQuiz</h1>
-          <p className="text-base">Please write your username below </p>
-          <div className="flex justify-center gap-10 items-center mt-3">
+      <main className="flex justify-center items-center min-h-screen font-mono bg-blue-200">
+        <div className="max-w-3xl mx-auto p-4">
+          <p className="mb-4 text-center sm:text-left">"{currentFact}"</p>
+          <h1 className=" mb-4 text-2xl font-extrabold text-center sm:text-left">Welcome to GoQuiz</h1>
+          <p className="text-center sm:text-left mb-4">Please write your username below </p>
+          <div className="flex flex-col sm:flex-row items-center gap-4 md:gap-10">
             <input
               id="userNameInput"
               name="userNameInput"
